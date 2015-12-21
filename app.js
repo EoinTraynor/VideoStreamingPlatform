fe2236fae7c929fd3d1a41e8a3638dc8d7d9832f
EoinTraynor <eointraynor1@gmail.com>
2015-12-21 23:38:22 +0000

    Updated the view engine to EJS

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -17,11 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// serve static html page
-app.get( '/', function( req, res ) {
-    res.sendFile( path.join( __dirname, 'public', 'index.html' ));
-});
 
 app.use('/', routes);
 app.use('/users', users);
