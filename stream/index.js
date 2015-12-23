5d3bbd4fbd005a8325d78e3e67be582d728a1ee9
EoinTraynor <eointraynor1@gmail.com>
2015-12-23 00:27:55 +0000

    Created new module and view for streams

---
+++ stream/index.js
@@ -0,0 +1,35 @@
+var name, catagory, subCatagory, private, sessionId, apiKey, token;
+
+exports.setName = function(nm){
+	name = nm;
+};
+// exports.setCatagory = function(ct){
+// 	catagory = ct;
+// };
+// exports.setSubCatagory = function(sct){
+// 	subCatagory = sct;
+// };
+// exports.setPrivate = function(pri){
+// 	private = pri;
+// };
+// exports.setSessionId = function(sid){
+// 	sessionId = sid;
+// };
+// exports.setApiKey = function(api){
+// 	apiKey = api;
+// };
+// exports.setToken = function(tk){
+// 	token = tk;
+// };
+
+exports.getInfo = function() {
+		return {
+			name: name,
+			// catagory: catagory,
+			// subCatagory: subCatagory,
+			// private: private,
+			// sessionId: sessionId,
+			// apiKey: apiKey,
+			// token: token
+		};
+};
