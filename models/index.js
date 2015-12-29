e5a45d1181c03abef818b042fae92625235fc52e
EoinTraynor <eointraynor1@gmail.com>
2015-12-29 20:31:47 +0000

    reconfig of stream model

--- models/index.js
+++ models/index.js
@@ -1,19 +1,24 @@
+// define stream model
+// specify the data attributes
 var Stream = function () {
 	this.data = {
 			sessionId: null,
 			apiKey: null,
-			token: null,
-			name: null,
-			catagory: null,
-			subCatagory: null,
-			private: null
+			//token: null,
+			//name: null,
+			//catagory: null,
+			//subCatagory: null,
+			//private: null
 	};
 
+	// confirm all data fields have a defined value
 	this.fill = function (info) {
 			for(var prop in this.data) {
 				if(this.data[prop] !== 'undefined'){
 					this.data[prop] = info[prop];
 				}
+				// else return error
+				//res.status(404).json({status:'error'});
 			}
 	};
 
@@ -22,10 +27,31 @@ var Stream = function () {
 	};
 };
 
-module.exports = function (info) {
+// array of streams
+var streams = [];
+
+module.exports.findAllPublicStreams = function() {
+	console.log("function: findAllPublicStreams");
+}
+
+module.exports.findStreamsByCat = function(cat) {
+	console.log("function: findStreamsByCat");
+}
+
+module.exports.findStreamsByName = function(name) {
+	console.log("function: findStreamsByName");
+}
+
+module.exports.findStreamsById = function(id) {
+	console.log("function: findStreamsById");
+}
+
+module.exports.newStream = function (info) {
 	var instance = new Stream();
-
 	instance.fill(info);
-
 	return instance;
 };
+
+module.exports.deleteStream = function() {
+	console.log("function: deleteStream");
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var stream = require('../models/index');
+//var streams = require('../models/streams');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,25 +13,28 @@ router.get('/', function(req, res, next) {
 /* GET a json of all streams */
 router.get('/streams', function(req, res){
     console.log('Getting all streams');
-    res.json({ message: 'List of Streams!' });
+    stream.findAllPublicStreams();
 });
 
 /* GET a json of strams relating to a specific catagory */
 //**
 router.get('/streams/catagory/:catagory', function(req, res){
     console.log('Getting streams relating to specific catagory');
+    stream.findStreamsByCat();
     res.json({ message: 'Get stream by catagory' });
 });
 
 /* GET a list strams relating to a specific search */
 router.get('/streams/search/:query', function(req, res){
     console.log('Getting all streams relating to a specific search');
+    stream.findStreamsByName();
     res.json({ message: 'Get stream by search' });
 });
 
 /* GET a specific stream */
 router.get('/streams/:id', function(req, res){
     console.log('Getting all streams by session id');
+    stream.findStreamsById();
     res.json({ message: 'Get specific stream!' });
 });
 
@@ -38,18 +42,14 @@ router.get('/streams/:id', function(req, res){
 //**
 router.post('/streams/:id', function(req, res){
     console.log('Creating a new stream');
+    stream.newStream();
     res.json({ message: 'Create new stream!' });
-    // create new stream & define attributes
-    // add stream to array of streams
-
-    stream.setName('Liverpool');
-    console.log(stream.getInfo());
-    //res.json({ message: 'Create new stream!' });
 });
 
 /* DELETE a specific stream */
 router.delete('/streams/:id', function(req, res){
     console.log('Deleting a specific stream');
+    stream.deleteStream();
     res.json({ message: 'Delete specific stream!' });
 });
 
