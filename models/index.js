842c89f3eedfaf0660846f46a9a1b4f8c7a3c666
EoinTraynor <eointraynor1@gmail.com>
2016-01-05 01:20:32 +0000

    CRD functionality implemented

--- models/index.js
+++ models/index.js
@@ -1,57 +1,88 @@
 // define stream model
 // specify the data attributes
-var Stream = function () {
-	this.data = {
-			sessionId: null,
-			apiKey: null,
-			//token: null,
-			//name: null,
-			//catagory: null,
-			//subCatagory: null,
-			//private: null
-	};
-
-	// confirm all data fields have a defined value
-	this.fill = function (info) {
-			for(var prop in this.data) {
-				if(this.data[prop] !== 'undefined'){
-					this.data[prop] = info[prop];
-				}
-				// else return error
-				//res.status(404).json({status:'error'});
-			}
-	};
-
-	this.getInformation = function () {
-		return this.data;
-	};
-};
+function stream(info) {
+	this.sessionId = info.sessionId;
+	this.apiKey = info.apiKey;
+	this.token = info.token;
+	this.name = info.name;
+	this.catagory = info.catagory;
+	this.subCatagory = info.subCatagory;
+	this.private = info.private;
+}
 
 // array of streams
-var streams = [];
+var streams = [
+	{
+		"sessionId": 11,
+	    "apiKey": 12,
+	    "token": 123,
+	    "name": 'stream1',
+	    "catagory": "football",
+	    "subCatagory": "sport",
+	    "private": "false"
+	},
+	{
+		"sessionId": 33,
+	    "apiKey": 132048,
+	    "token": 132409,
+	    "name": 'stream2',
+	    "catagory": "football",
+	    "subCatagory": "sport",
+	    "private": "false"
+	}
+];
 
 module.exports.findAllPublicStreams = function() {
-	console.log("function: findAllPublicStreams");
+	var result = [];
+	for(var s in streams){
+		if (streams[s].private == "false") {
+			result.push(streams[s]);
+		}
+	}
+	return result;
 }
 
 module.exports.findStreamsByCat = function(cat) {
-	console.log("function: findStreamsByCat");
+	var result = [];
+	for(var s in streams){
+		if (streams[s].catagory == cat) {
+			result.push(streams[s]);
+		}
+	}
+	return result;
 }
 
 module.exports.findStreamsByName = function(name) {
-	console.log("function: findStreamsByName");
+	var result = [];
+	for(var s in streams){
+		if (streams[s].name == name) {
+			result.push(streams[s]);
+		}
+	}
+	return result;
 }
 
 module.exports.findStreamsById = function(id) {
-	console.log("function: findStreamsById");
+	var result = [];
+	for(var s in streams){
+		if (streams[s].sessionId == id) {
+			result.push(streams[s]);
+		}
+	}
+	return result;
 }
 
 module.exports.newStream = function (info) {
-	var instance = new Stream();
-	instance.fill(info);
+	var instance = new stream(info);
+	streams.push(instance);
 	return instance;
 };
 
-module.exports.deleteStream = function() {
-	console.log("function: deleteStream");
+module.exports.deleteStream = function(id) {
+	for(var s in streams){
+		if (streams[s].sessionId == id) {
+			//result.push(streams[s]);
+			streams.splice(streams[s]);
+		}
+	}
 }
--- routes/index.js
+++ routes/index.js
@@ -7,50 +7,43 @@ var stream = require('../models/index');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
-  //res.json({foo: 'bar'});
 });
 
-/* GET a json of all streams */
+/* GET a json of all public streams */
 router.get('/streams', function(req, res){
-    console.log('Getting all streams');
-    stream.findAllPublicStreams();
+    res.json(stream.findAllPublicStreams());
 });
 
 /* GET a json of strams relating to a specific catagory */
 //**
 router.get('/streams/catagory/:catagory', function(req, res){
     console.log('Getting streams relating to specific catagory');
-    stream.findStreamsByCat();
-    res.json({ message: 'Get stream by catagory' });
+    res.json(stream.findStreamsByCat(req.params.catagory));
 });
 
 /* GET a list strams relating to a specific search */
 router.get('/streams/search/:query', function(req, res){
     console.log('Getting all streams relating to a specific search');
-    stream.findStreamsByName();
-    res.json({ message: 'Get stream by search' });
+    res.json(stream.findStreamsByName(req.params.query));
 });
 
 /* GET a specific stream */
 router.get('/streams/:id', function(req, res){
     console.log('Getting all streams by session id');
-    stream.findStreamsById();
-    res.json({ message: 'Get specific stream!' });
+    res.json(stream.findStreamsById(req.params.id));
 });
 
 /* POST a new stream */
 //**
-router.post('/streams/:id', function(req, res){
+router.post('/streams', function(req, res){
     console.log('Creating a new stream');
-    stream.newStream();
-    res.json({ message: 'Create new stream!' });
+    res.json(stream.newStream(req.body));
 });
 
 /* DELETE a specific stream */
 router.delete('/streams/:id', function(req, res){
     console.log('Deleting a specific stream');
-    stream.deleteStream();
-    res.json({ message: 'Delete specific stream!' });
+    res.json(stream.deleteStream(req.params.id));
 });
 
 module.exports = router;
