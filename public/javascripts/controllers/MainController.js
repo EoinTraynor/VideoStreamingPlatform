786790383a2e719d433cd662bb89ffcfef40adb0
EoinTraynor <eointraynor1@gmail.com>
2016-02-11 15:58:19 +0000

    Angular: API service factory and controller & controller setup

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -1,9 +1,36 @@
-// app.controller('mainController', ['$scope', 'searchAll',
-// function($scope, search) {
-// 	search.success(function(data) {
-// 		$scope.streams = data;
-// 	});
-// }]);
+function mainController ($scope, streamFactory) {
 
-// use joes controller = https://github.com/JosephOR/AngularMovieSearch/blob/master/js/controllers/MovieController.js
-app.controller();
+	streamFactory.getStreams()
+	.then(function (data) {
+		$scope.streams = data;
+	})
+	.catch(function (err) {
+		$scope.status = err.message;
+	});
+
+	// streamFactory.success(function(data) {
+	// 	$scope.streams = data;
+	// });
+};
+
+app.controller('mainController', mainController);
+
+// use mainController controller here = https://github.com/JosephOR/AngularMovieSearch/blob/master/js/controllers/MovieController.js
+
+// function mainController($scope, streamFactory) {
+//
+// 	streamFactory.getStreams(function (data) {
+// 		$scope.streams = data;
+// 	},
+// 	function () {
+//
+// 	});
+//
+// 	$scope.getStream();
+// 	$scope.getSearch();
+// 	$scope.getCatagory();
+// 	$scope.postStream();
+// 	$scope.deleteStream();
+// }
+//
+// app.controller('mainController', mainController);
--- public/javascripts/services/queryAPI.js
+++ public/javascripts/services/queryAPI.js
@@ -1,22 +1,38 @@
-app.factory('searchAll', ['$http', function($http) {
-	return $http.get('/streams')
-	.success(function(data){
-		return data;
-		console.log(data);
-	})
-	.error(function(err){
-		return err;
-	});
+app.factory('streamFactory', ['$http', '$q', function($http, $q) {
+
+	var dataFactory = {};
+
+	dataFactory.getStreams = function () {
+		var defer = $q.defer();
+
+		$http.get('/streams').success(function (data) {
+			defer.resolve(data);
+		})
+		.error(function (err) {
+			defer.reject(err);
+		});
+
+		return defer.promise;
+	}
+
+	// return $http.get('/streams')
+	// .success(function(data){
+	// 	return data;
+	// 	console.log(data);
+	// })
+	// .error(function(err){
+	// 	return err;
+	// });
+
+	return dataFactory;
 }]);
 
-app.factory('searchOne', ['$http', function ($http) {
-	return $http.get('/stream:id')
-	.success(function(data) {
-			return(data);
-			console.log(data);
-	})
-	.error(function (err) {
-		return err;
-		console.log(err);
-	});
-}]);
+
+// function streamFactory($scope, $http) {
+// 	var getStreams = function (cb, err){
+// 		return $http.get('/streams').then(cb, err);
+// 	};
+// 	return {getStreams: getStreams};
+// }
+//
+// app.factory('streamFactory', streamFactory);
