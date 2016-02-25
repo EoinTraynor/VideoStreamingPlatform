088c92156437c62a50fc5484186a9b8bdecb6397
EoinTraynor <eointraynor1@gmail.com>
2016-02-25 00:10:01 +0000

    OpenkTok: Working multicast

--- public/javascripts/openTok/index.js
+++ public/javascripts/openTok/index.js
@@ -4,113 +4,68 @@ function opentokConfig(obj) {
     var apiKey = obj.apiKey;
     var token = obj.token;
     var hostIdentified = false;
+    var connectionCount=0;
 
-    var targetElement = 'video-stream';
-    var connectionCount = 0;
-
+    // check if browser supports WebRTC
+    // WebRTC supported; initialise a session
     var session = OT.initSession(apiKey, sessionId);
-
-    session.on('streamCreated', function(event) {
-        session.subscribe(event.stream, 'video-stream');
+    session.on({
+        // new client joins the session
+        connectionCreated: function(event) {
+            connectionCount++;
+            console.log("connectionCount " + connectionCount);
+        },
+        // client leaves the session
+        connectionDestroyed: function connectionDestroyedHandler(event) {
+            console.log("client disconnects for stream");
+            if (event.reason == "networkDisconnected") {
+                console.log("Your network connection terminated");
+            }
+            connectionCount--;
+            console.log("connectionCount " + connectionCount);
+        },
+        // cliet subscribes to stream
+        streamCreated: function(event) {
+            console.log("cliet subscribes to stream");
+            session.subscribe(event.stream, 'video-stream', function(err) {
+                if (!err) {console.log("New stream");}
+                else {console.log("client failed to read stream");}
+            });
+        },
+        streamDestroyed: function(event) {
+            console.log("Stream Destroyed");
+        },
+        sessionDisconnected: function sessionDisconnectedHandler(event) {
+            console.log("Session Disconnected");
+            if (event.reason == "networkDisconnected") {
+                console.log("networkDisconnected");
+            }
+        }
     });
 
     session.connect(token, function(err) {
         if (!err) {
             if (hostIdentified) {
-                var publisher = OT.initPublisher('video-stream');
+                var publisherProperties = {resolution: '1280x720'};
+                var publisher = OT.initPublisher('video-stream', publisherProperties);
                 session.publish(publisher);
+                console.log("Publishing Stream");
+                publisher.on({
+                    streamCreated: function(event) {
+                        console.log("resolution " + event.stream.videoDimensions.height);
+                    },
+                    streamDestroyed: function(event) {
+                        console.log("publisher stopped recording");
+                        disconnect();
+                    }
+                });
             }
         }
         else {console.log("Error Connecting");}
     });
-    // function connect() {
-    //     // check if browser does not support WebRTC
-    //     if (OT.checkSystemRequirements() == 1) {
-    //         // WebRTC supported; initialise a session
-    //         var session = OT.initSession(apiKey, sessionId);
-    //         console.log("session initialised");
-    //
-    //         // while the connection is open
-    //         session.on({
-    //             // new client peer connects
-    //             connectionCreated: function(event) {
-    //                 session.subscribe(event.stream, targetElement, function(err) {
-    //                     if (err) {console.log(err);}
-    //                     else { console.log("Subscriber added");}
-    //                 });
-    //                 connectionCount++;
-    //                 console.log(connectionCount + " peers");
-    //             },
-    //             // client peer disconnects
-    //             connectionDestroyed: function(event) {
-    //                 connectionCount--;
-    //                 console.log(connectionCount + " peers");
-    //             },
-    //             // host disconnects
-    //             sessionDisconnected: function sessionDisconnectedHandler(event) {
-    //                 console.log("Disconnected from the Session");
-    //                 if (event.reason == 'networkDisconnected') {
-    //                     console.log('You network connection terminated');
-    //                 }
-    //             }
-    //         });
-    //
-    //         // connect to the session
-    //         session.connect(token, function(err) {
-    //             if (err) { console.log("Error connecting: " + err.message);}
-    //             else {
-    //                 console.log("Connected to session");
-    //                 if (hostIdentified) {
-    //                     console.log("Host already Identified");
-    //                 }
-    //                 else {
-    //                     console.log("establishing host");
-    //                     publishStream(session);
-    //                 }
-    //             }
-    //         });
-    //
-    //     }
-    //
-    //     // WebRTC not supported
-    //     else {console.log("WebRTC not supported");}
-    //
-    // }
-    //
-    // function publishStream(session) {
-    //     var publisher;
-    //
-    //     // check if host has publish capabilities
-    //     if (session.capabilities.publish == 1) {
-    //         // attempt to initialise publish
-    //         publisher = OT.initPublisher(targetElement, null, function(err) {
-    //             if (err) {
-    //                 // publish unsuccessful
-    //                 console.log("publish unsuccessful");
-    //             }
-    //             else {
-    //                 // publish successful
-    //                 console.log("publish successful");
-    //             }
-    //         });
-    //         // Detect result of host cam & mic access
-    //         publisher.on({
-    //             accessAllowed: function(event) {
-    //                 // user has granted access
-    //                 console.log("Access to cam & mic");
-    //             },
-    //             accessDenied: function accessDeniedHandler(event) {
-    //                 console.log("Access denied");
-    //             }
-    //         });
-    //     }
-    //     // client unable to publish
-    //     else {console.log("client unable to publish");}
-    // };
-    //
-    // function disconnect() {
-    //     session.disconnect();
-    // };
-    // connect();
+
+    function disconnect() {
+        session.disconnect();
+    }
 
 };
